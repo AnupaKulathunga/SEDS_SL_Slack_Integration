8956a8ae6bc2d340edb57b9b886a254a80753531
AnupaKulathunga <pmadnkulathunga@gmail.com>
2021-06-01 20:23:42 +0530

    d

--- test.py
+++ test.py
@@ -2,10 +2,18 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+app =Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET_'], '/slack/events', app)
+
 client =  slack.WebClient(token=os.environ['SLACK_TOKEN_'])
 
-client.chat_postMessage(channel='seds_sl_slack_intergration', text="Hello World!")
+client.chat_postMessage(channel='#test', text="Hello World!")
+
+if __name__ == "__main__":
+    app.run(debug=True)

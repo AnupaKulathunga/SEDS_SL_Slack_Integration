19dc2cdcb082265c73604757a1a90878f182cd4f
AnupaKulathunga <pmadnkulathunga@gmail.com>
2021-06-02 05:43:36 +0530

    d

--- app.py
+++ app.py
@@ -2,128 +2,82 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask, request, Response
+from flask import Flask
 from slackeventsapi import SlackEventAdapter
-import string
-from datetime import datetime, timedelta
-import time
 
-load_dotenv()
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(
-    os.environ['SIGNING_SECRET_'], '/slack/events', app)
+app =Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET_'], '/slack/events', app)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN_'])
+client =  slack.WebClient(token=os.environ['SLACK_TOKEN_'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
-message_counts = {}
-welcome_messages = {}
-
-BAD_WORDS = ['hmm', 'no', 'tim']
-
-SCHEDULED_MESSAGES = [
-    {'text': 'First message', 'post_at': (
-        datetime.now() + timedelta(seconds=20)).timestamp(), 'channel': 'C01BXQNT598'},
-    {'text': 'Second Message!', 'post_at': (
-        datetime.now() + timedelta(seconds=30)).timestamp(), 'channel': 'C01BXQNT598'}
-]
-
-
-class WelcomeMessage:
-    START_TEXT = {
-        'type': 'section',
-        'text': {
-            'type': 'mrkdwn',
-            'text': (
-                'Welcome to this awesome channel! \n\n'
-                '*Get started by completing the tasks!*'
-            )
-        }
-    }
-
-    DIVIDER = {'type': 'divider'}
-
-    def __init__(self, channel):
-        self.channel = channel
-        self.icon_emoji = ':robot_face:'
-        self.timestamp = ''
-        self.completed = False
-
-    def get_message(self):
-        return {
-            'ts': self.timestamp,
-            'channel': self.channel,
-            'username': 'Welcome Robot!',
-            'icon_emoji': self.icon_emoji,
-            'blocks': [
-                self.START_TEXT,
-                self.DIVIDER,
-                self._get_reaction_task()
-            ]
-        }
-
-    def _get_reaction_task(self):
-        checkmark = ':white_check_mark:'
-        if not self.completed:
-            checkmark = ':white_large_square:'
-
-        text = f'{checkmark} *React to this message!*'
-
-        return {'type': 'section', 'text': {'type': 'mrkdwn', 'text': text}}
-
-
-def send_welcome_message(channel, user):
-    if channel not in welcome_messages:
-        welcome_messages[channel] = {}
-
-    if user in welcome_messages[channel]:
-        return
-
-    welcome = WelcomeMessage(channel)
-    message = welcome.get_message()
-    response = client.chat_postMessage(**message)
-    welcome.timestamp = response['ts']
-
-    welcome_messages[channel][user] = welcome
-
-
-def list_scheduled_messages(channel):
-    response = client.chat_scheduledMessages_list(channel=channel)
-    messages = response.data.get('scheduled_messages')
-    ids = []
-    for msg in messages:
-        ids.append(msg.get('id'))
-
-    return ids
-
-
-def schedule_messages(messages):
-    ids = []
-    for msg in messages:
-        response = client.chat_scheduleMessage(
-            channel=msg['channel'], text=msg['text'], post_at=msg['post_at']).data
-        id_ = response.get('scheduled_message_id')
-        ids.append(id_)
-
-    return ids
-
-
-def delete_scheduled_messages(ids, channel):
-    for _id in ids:
-        try:
-            client.chat_deleteScheduledMessage(
-                channel=channel, scheduled_message_id=_id)
-        except Exception as e:
-            print(e)
-
-
-def check_if_bad_words(message):
-    msg = message.lower()
-    msg = msg.translate(str.maketrans('', '', string.punctuation))
-
-    return any(word in msg for word in BAD_WORDS)
-
+#client.chat_postMessage(channel='#test', text="Hello World!")
+client.chat_postMessage(
+  channel="seds_sl_slack_intergration",
+  blocks=[
+    {
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Hey there 👋 I'm TaskBot. I'm here to help you create and manage tasks in Slack.\nThere are two ways to quickly create tasks:"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*1️⃣ Use the `/task` command*. Type `/task` followed by a short description of your tasks and I'll ask for a due date (if applicable). Try it out by using the `/task` command in this channel."
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*2️⃣ Use the _Create a Task_ action.* If you want to create a task from a message, select `Create a Task` in a message's context menu. Try it out by selecting the _Create a Task_ action for this message (shown below)."
+			}
+		},
+		{
+			"type": "image",
+			"title": {
+				"type": "plain_text",
+				"text": "image1",
+				"emoji": True
+			},
+			"image_url": "https://api.slack.com/img/blocks/bkb_template_images/onboardingComplex.jpg",
+			"alt_text": "image1"
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "➕ To start tracking your team's tasks, *add me to a channel* and I'll introduce myself. I'm usually added to a team or project channel. Type `/invite @TaskBot` from the channel or pick a channel on the right."
+			},
+			"accessory": {
+				"type": "conversations_select",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select a channel...",
+					"emoji": True
+				}
+			}
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "context",
+			"elements": [
+				{
+					"type": "mrkdwn",
+					"text": "👀 View all tasks with `/task list`\n❓Get help at any time with `/task help` or type *help* in a DM with me"
+				}
+			]
+		}
+  ]
+)
 
 @ slack_event_adapter.on('message')
 def message(payload):
@@ -133,50 +87,9 @@ def message(payload):
     text = event.get('text')
 
     if user_id != None and BOT_ID != user_id:
-        if user_id in message_counts:
-            message_counts[user_id] += 1
-        else:
-            message_counts[user_id] = 1
+        client.chat_postMessage(channel='#test', text=text)
 
-        if text.lower() == 'start':
-            send_welcome_message(f'@{user_id}', user_id)
-        elif check_if_bad_words(text):
-            ts = event.get('ts')
-            client.chat_postMessage(
-                channel=channel_id, thread_ts=ts, text="THAT IS A BAD WORD!")
-
-
-@ slack_event_adapter.on('reaction_added')
-def reaction(payload):
-    event = payload.get('event', {})
-    channel_id = event.get('item', {}).get('channel')
-    user_id = event.get('user')
-
-    if f'@{user_id}' not in welcome_messages:
-        return
-
-    welcome = welcome_messages[f'@{user_id}'][user_id]
-    welcome.completed = True
-    welcome.channel = channel_id
-    message = welcome.get_message()
-    updated_message = client.chat_update(**message)
-    welcome.timestamp = updated_message['ts']
-
-
-@ app.route('/message-count', methods=['POST'])
-def message_count():
-    data = request.form
-    user_id = data.get('user_id')
-    channel_id = data.get('channel_id')
-    message_count = message_counts.get(user_id, 0)
-
-    client.chat_postMessage(
-        channel=channel_id, text=f"Message: {message_count}")
-    return Response(), 200
-
+#@ app.
 
 if __name__ == "__main__":
-    schedule_messages(SCHEDULED_MESSAGES)
-    ids = list_scheduled_messages('C01BXQNT598')
-    delete_scheduled_messages(ids, 'C01BXQNT598')
     app.run(debug=True)
